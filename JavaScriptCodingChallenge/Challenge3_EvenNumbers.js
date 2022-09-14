6cc18fb23c2f0d3033a53710c410ddb9793d18b7
Olamide John <olamide.john@outlook.com>
2022-09-15 00:24:41 +0100

    edit branch

--- JavaScriptCodingChallenge/Challenge3_EvenNumbers.js
+++ JavaScriptCodingChallenge/Challenge3_EvenNumbers.js
@@ -1,6 +1,6 @@
 //prints out all the Even numbers between 0 to 10
 
-for(let number = 0; number <= 10; number = number +1){ //initiializing the start point, state the condition and increament
+for(let number = 0; number <= 100; number = number +1){ //initiializing the start point, state the condition and increament
     if(number % 2 === 0){
         console.log(number +' is an Even number')
     }
